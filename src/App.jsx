e4dba5aa799c2444a7a263d20ed93a55a2c465cc
nimaester <nimaester@gmail.com>
2020-11-16 23:58:50 -0800

    added App component

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,24 @@ import React, { useState, useRef, useEffect } from 'react';
 
 const App = () => {
   return (
-    <div>
+    <div className="main">
       <h1>Hello World</h1>
+      <div>
+        <ul>
+          <li className="nav">
+            Home
+          </li>
+          <li className="nav">
+            Contacts
+          </li>
+          <li className="nav">
+            User
+          </li>
+          <li className="nav">
+            About
+          </li>
+        </ul>
+      </div>
     </div>
   );
 };
