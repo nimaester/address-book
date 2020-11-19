9212062ad58a9bebf7e475f6af46d0648a10a17e
nimaester <nimaester@gmail.com>
2020-11-19 01:30:41 -0800

    created context api for contacts

--- src/context/contact/ContactState.js
+++ src/context/contact/ContactState.js
@@ -2,4 +2,15 @@ import React, { useReducer } from 'react';
 import uuid from 'uuid';
 import ContactContext from './contactContext';
 import contactReducer from './contactReducer';
+import {
+  ADD_CONTACT,
+  DELETE_CONTACT,
+  SET_CURRENT,
+  CLEAR_CURRENT,
+  UPDATE_CONTACT,
+  FILTER_CONTACTS,
+  CLEAR_FILTER,
+  SET_ALERT,
+  REMOVE_ALERT
+} from '../types';
 
