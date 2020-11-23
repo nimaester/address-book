7d0180272bfb66cd1ffd0ee1e9db91ac7774774d
nimaester <nimaester@gmail.com>
2020-11-23 12:12:53 -0800

    added an about page

---
+++ client/src/About.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const About = () => {
+  return (
+    <div>
+      <h1>About this App</h1>
+      <p className="my-1">
+        Full stack react app that stores contacts
+      </p>
+      <p>
+        <strong>
+          Version:
+        </strong>
+        1.0.0
+      </p>
+    </div>
+  );
+};
+
+export default About;
--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,24 @@
-import React from 'react';
+import React, {Fragment} from 'react';
 import './App.css';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Navbar from './Navbar';
+import Home from './Home';
+import About from './About';
 
-function App() {
+const App = () => {
+
   return (
-    <div className="App">
-      <h1>Address Book</h1>
-    </div>
+    <Router>
+      <Fragment>
+        <Navbar />
+        <div className="container">
+          <Switch>
+            <Route exact path ='/' component={Home} />
+            <Route exact path ='/About' component={About} />
+          </Switch>
+        </div>
+      </Fragment>
+    </Router>
   );
 }
 
