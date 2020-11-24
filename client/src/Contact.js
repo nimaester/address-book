fe570b1d33c674ed6ba94a4d7b2c87c546d14a02
nimaester <nimaester@gmail.com>
2020-11-24 11:43:00 -0800

    added contacts

--- client/src/Contact.js
+++ client/src/Contact.js
@@ -1,11 +1,19 @@
 import React from 'react';
 
-const Contact = () => {
+const Contact = ({contact}) => {
+  const {id, name, email, type} = contact;
   return (
-    <div>
-      Contact
+    <div className='card bg-light'>
+      <h3 className='text-primary text-left'>
+        {name}{' '}
+        <span className={'badge' + (type === 'professional' ? 'badge-success' : 'badge-primary')}>{type}
+
+        </span>
+      </h3>
     </div>
   );
 };
+
+
 
 export default Contact;
