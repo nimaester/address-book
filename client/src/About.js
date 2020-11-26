864da70dd5d0e7d81d13de48b9873fe92755ea46
nimaester <nimaester@gmail.com>
2020-11-25 22:58:41 -0800

    updated a few css

--- client/src/About.js
+++ client/src/About.js
@@ -5,7 +5,7 @@ const About = () => {
     <div>
       <h1>About this App</h1>
       <p className="my-1">
-        Full stack react app that stores contacts
+        A fullstack react application that stores contacts. It also uses all crud operations (Create, Read, Update, Delete) that is using MongoAtlas as its backend to store a user's list of contacts.
       </p>
       <p>
         <strong>
