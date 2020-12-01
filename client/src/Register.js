4238ea95f8eb64b2461ef18a58688e6323e86780
nimaester <nimaester@gmail.com>
2020-12-01 02:55:44 -0800

    added few more type

--- client/src/Register.js
+++ client/src/Register.js
@@ -1,6 +1,11 @@
-import React, { useState } from "react";
+import React, { useState, useContext } from "react";
+import AlertContext from "./context/alert/alertContext";
 
 const Register = () => {
+
+  const alertContext = useContext(AlertContext);
+  const setAlert = alertContext;
+
   const [user, setUser] = useState({
     name: "",
     email: "",
@@ -19,7 +24,14 @@ const Register = () => {
 
   const handleSubmit = (event) => {
     event.preventDefault();
-    console.log("Register submitted");
+    if (name === '' || email === '' || password === '') {
+      setAlert('Please enter all fields', 'danger')
+    } else if (password !== password2) {
+      setAlert('Passwords do not match', 'danger')
+    } else {
+      console.log("Register submitted");
+    }
+
   };
 
   return (
