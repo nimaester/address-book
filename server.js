d148c1eb640fb5f6d5b3332d9078a50add3133e4
nimaester <nimaester@gmail.com>
2021-01-06 16:47:37 -0800

    finished all crud ops

--- server.js
+++ server.js
@@ -12,6 +12,16 @@ app.use('/api/users', require('./routes/users'));
 app.use('/api/contacts', require('./routes/contacts'));
 app.use('/api/auth', require('./routes/auth'));
 
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+  app.get('*', (req, res) => (
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  ))
+} else {
+
+}
+
+
 app.listen(PORT, () => {
   console.log(`Server running on ${PORT}`);
 });
