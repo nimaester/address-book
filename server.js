a06bd3da40e1241cad0b4e22f49f06f50024c2f0
nimaester <nimaester@gmail.com>
2021-01-06 17:05:49 -0800

    updated files for heroku deployment

--- server.js
+++ server.js
@@ -18,6 +18,7 @@ if (process.env.NODE_ENV === 'production') {
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
   ))
 }
+
 app.listen(PORT, () => {
   console.log(`Server running on ${PORT}`);
 });
